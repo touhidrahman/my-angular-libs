9d3c1cf65160dc9f4e172ff64109b317ea97a5a6
istiuak <istiuak.0@gmail.com>
2026-07-27 04:57:00 +0600

    add session sotrage

---
+++ projects/ngx-common/src/lib/services/session-storage.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, InjectionToken, inject } from '@angular/core'
+import { WA_SESSION_STORAGE } from '@ng-web-apis/common'
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SessionStorageService {
+   //note:: angular version mismatch giving type error
+    private storage = inject(
+        WA_SESSION_STORAGE as unknown as InjectionToken<Storage>
+    )
+
+    clear(): void {
+        this.storage.clear()
+    }
+
+    getItem(key: string): string | null {
+        return this.storage.getItem(key)
+    }
+
+    key(index: number): string | null {
+        return this.storage.key(index)
+    }
+
+    removeItem(key: string): void {
+        this.storage.removeItem(key)
+    }
+
+    setItem(key: string, value: string): void {
+        this.storage.setItem(key, value)
+    }
+}
